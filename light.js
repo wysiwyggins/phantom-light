2d38dc2113e5a3cbbb8c825e2e6d2fda27bc706e
Wiley Wiggins <weevil@wileywiggins.com>
2023-02-24 21:40:49 -0800

    Update light.js

--- light.js
+++ light.js
@@ -1,44 +1,37 @@
 //light.js would set a dmx rgb fill light based on the current room color of the mqtt-client middleware's 
 //token-connected character at mudroom.rip
-//the topic will be called dmx/set
-
-const opendmx = require('opendmx');
+//the topic will be called dmx/setconst mqtt = require('mqtt');
 const mqtt = require('mqtt');
+const DMX = require('dmx');
 
-// DMX configuration
-const universe = 0;
-const channels = 3;
+// Connect to Mosquitto Broker
+const client = mqtt.connect('mqtt://localhost');
 
-// MQTT configuration
-const brokerUrl = 'mqtt://localhost:1883';
-const topic = 'dmx/set';
+// Initialize DMX universe and device
+const dmx = new DMX();
+const universe = dmx.addUniverse('myUniverse', 'enttec-usb-dmx-pro', '/dev/ttyUSB0');
 
-// Connect to the DMX light
-opendmx.init('/dev/ttyUSB0', universe);
-
-// Connect to the MQTT broker
-const client = mqtt.connect(brokerUrl);
-
-// Subscribe to the topic
+// Subscribe to Mosquitto topic
 client.on('connect', () => {
-  console.log(`Connected to MQTT broker at ${brokerUrl}`);
-  client.subscribe(topic, (err) => {
-    if (err) {
-      console.error(`Error subscribing to topic ${topic}: ${err}`);
-    } else {
-      console.log(`Subscribed to topic ${topic}`);
-    }
-  });
+  client.subscribe('dmx/set');
 });
 
-// Handle incoming messages
+// Handle messages received from Mosquitto topic
 client.on('message', (topic, message) => {
-  const payload = JSON.parse(message.toString());
-  if (Array.isArray(payload) && payload.length === channels) {
-    opendmx.setChannel(universe, 1, payload[0]);
-    opendmx.setChannel(universe, 2, payload[1]);
-    opendmx.setChannel(universe, 3, payload[2]);
-  } else {
-    console.warn(`Invalid payload received: ${message}`);
+  if (topic === 'dmx/set') {
+    // Parse message to get hex color value
+    const hexColor = message.toString();
+
+    // Convert hex color value to RGB values
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+
+    // Set DMX channels for RGB values
+    universe.update({
+      1: r,
+      2: g,
+      3: b
+    });
   }
 });
