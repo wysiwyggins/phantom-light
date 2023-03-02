acb3f3dc6c3b94e504fce88e96649ecd6b779314
Wiley Wiggins <weevil@wileywiggins.com>
2023-03-02 14:13:29 -0800

    Update light.js

--- light.js
+++ light.js
@@ -1,37 +1,40 @@
 //light.js would set a dmx rgb fill light based on the current room color of the mqtt-client middleware's 
 //token-connected character at mudroom.rip
 //the topic will be called dmx/setconst mqtt = require('mqtt');
+
 const mqtt = require('mqtt');
 const DMX = require('dmx');
-
-// Connect to Mosquitto Broker
-const client = mqtt.connect('mqtt://localhost');
-
-// Initialize DMX universe and device
 const dmx = new DMX();
-const universe = dmx.addUniverse('myUniverse', 'enttec-open-usb-dmx', '/dev/ttyUSB1');
+const universe = dmx.addUniverse('default', 'enttec-open-usb-dmx', '/dev/ttyUSB1');
 
-// Subscribe to Mosquitto topic
-client.on('connect', () => {
-  client.subscribe('dmx/set');
-});
+const client  = mqtt.connect('mqtt://localhost');
+client.on('connect', function () {
+  client.subscribe('dmx/set', function (err) {
+    if (err) {
+      console.log('Error subscribing to MQTT topic:', err);
+    } else {
+      console.log('Subscribed to MQTT topic: dmx/set');
+    }
+  })
+})
 
-// Handle messages received from Mosquitto topic
-client.on('message', (topic, message) => {
+client.on('message', function (topic, message) {
   if (topic === 'dmx/set') {
-    // Parse message to get hex color value
     const hexColor = message.toString();
+    console.log('Received message:', hexColor);
 
-    // Convert hex color value to RGB values
-    const r = parseInt(hexColor.slice(1, 3), 16);
-    const g = parseInt(hexColor.slice(3, 5), 16);
-    const b = parseInt(hexColor.slice(5, 7), 16);
+    // Convert hex color to RGB
+    const red = parseInt(hexColor.slice(0,2), 16);
+    const green = parseInt(hexColor.slice(2,4), 16);
+    const blue = parseInt(hexColor.slice(4,6), 16);
 
-    // Set DMX channels for RGB values
-    universe.update({
-      1: r,
-      2: g,
-      3: b
+    // Set DMX values for RGB fill light
+    universe.update({1: red, 2: green, 3: blue}, function (err) {
+      if (err) {
+        console.log('Error setting DMX values:', err);
+      } else {
+        console.log('DMX values updated:', {red, green, blue});
+      }
     });
   }
-});
+})
