6207d9ed80b9b2843b08ac8448125d67a430da14
Wiley Wiggins <weevil@wileywiggins.com>
2023-03-02 15:58:38 -0800

    Update light.js

--- light.js
+++ light.js
@@ -12,17 +12,17 @@ client.on('connect', function () {
   client.subscribe('dmx/set');
 });
 client.on('message', (topic, message) => {
-  const hexColor = message.toString().replace('#', '');
-  if (!/^[0-9A-Fa-f]{6}$/g.test(hexColor)) {
-    console.error(`Invalid color value in message: ${message}`);
+  const hexColor = message.toString().trim();
+  if (!/^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/g.test(hexColor)) {
+    console.error(`Invalid color value in message: ${hexColor}`);
     return;
   }
 
-  const red = parseInt(hexColor.slice(0, 2), 16);
-  const green = parseInt(hexColor.slice(2, 4), 16);
-  const blue = parseInt(hexColor.slice(4, 6), 16);
+  const red = parseInt(hexColor.slice(1, 3), 16);
+  const green = parseInt(hexColor.slice(3, 5), 16);
+  const blue = parseInt(hexColor.slice(5, 7), 16);
 
-  console.log(`Received message: ${message}`);
+  console.log(`Received message: ${hexColor}`);
   console.log(`Setting DMX value to: ${red}, ${green}, ${blue}`);
 
   universe.update(1, [red, green, blue]);
