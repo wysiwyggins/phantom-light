4024d343800989a16164d76b1b5e9e394044abf9
Wiley Wiggins <weevil@wileywiggins.com>
2023-03-02 15:01:53 -0800

    Update light.js

--- light.js
+++ light.js
@@ -21,7 +21,7 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   if (topic === 'dmx/set') {
-    const hexColor = message.toString();
+    const hexColor = message;
     console.log('Received message:', hexColor);
 
     // Convert hex color to RGB
@@ -29,12 +29,10 @@ client.on('message', function (topic, message) {
     const green = parseInt(hexColor.slice(2, 4), 16);
     const blue = parseInt(hexColor.slice(4, 6), 16);
 
-    // Check for invalid RGB values
     if (isNaN(red) || isNaN(green) || isNaN(blue)) {
       console.log('Invalid RGB values received:', {red, green, blue});
       return;
     }
-
     // Set DMX values for RGB fill light
     universe.update({1: red, 2: green, 3: blue}, function (err) {
       if (err) {
